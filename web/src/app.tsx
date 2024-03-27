7827df28c98c34e7c8c723c05eb60f8c0ceeb80c
Tomáš Burian <tomas.burian@marius.eu>
2024-03-27 23:54:33 +0100

    Test commit

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client';
 const queryClient = new QueryClient();
 
 const App: FC = () => {
-	return <div></div>;
+	return <div>Hello world</div>;
 };
 
 createRoot(document.getElementById('root')!).render(<App />);
