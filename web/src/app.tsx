b53f7a61d7a7cf9e74a558d2b10bac5537e54efc
CzechCoder <burian.tom@hotmail.com>
2024-03-28 00:37:51 +0100

    add gitignore and install tailwind

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,11 +1,16 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { type FC } from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css';
 
 const queryClient = new QueryClient();
 
 const App: FC = () => {
-	return <div>Hello world again</div>;
+	return (
+		<h1 className='text-xl font-bold underline text-purple-500'>
+			Hello world again
+		</h1>
+	);
 };
 
 createRoot(document.getElementById('root')!).render(<App />);
